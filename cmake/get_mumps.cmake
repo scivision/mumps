9125517e662b6b2884ed308f6ef01688dc91b44d
Michael Hirsch <scivision@users.noreply.github.com>
2021-04-20 15:32:40 -0400

    add sha256 hash check

--- cmake/get_mumps.cmake
+++ cmake/get_mumps.cmake
@@ -1,12 +1,16 @@
 file(READ ${CMAKE_CURRENT_LIST_DIR}/mumps.json json)
 
-string(JSON url GET ${json} mumps url1)
-set(mumps_urls ${url})
-string(JSON url GET ${json} mumps url2)
-list(APPEND mumps_urls ${url})
+
+
+string(JSON url0 GET ${json} mumps url0)
+string(JSON url1 GET ${json} mumps url1)
+string(JSON url2 GET ${json} mumps url2)
+string(JSON mumps_sha256 GET ${json} mumps sha256)
+set(mumps_urls ${url0} ${url1} ${url2})
 
 FetchContent_Declare(mumps
 URL ${mumps_urls}
+URL_HASH SHA256=${mumps_sha256}
 TLS_VERIFY ON)
 
 if(NOT mumps_POPULATED)
