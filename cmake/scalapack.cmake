9610f14d6c07f07f30e8637edf0c340dcbaf89ab
Michael Hirsch <scivision@users.noreply.github.com>
2021-06-22 13:59:49 -0400

    scalapack 2.1.0.22 fixes clang, so remove mumps patch

--- cmake/scalapack.cmake
+++ cmake/scalapack.cmake
@@ -29,14 +29,6 @@ endif()
 
 set(scalapack_external true CACHE BOOL "build ScaLapack")
 
-set(scalapack_c_flags)
-include(CheckCompilerFlag)
-# test the non-no form, otherwise always succeeds
-check_compiler_flag(C -Wimplicit-function-declaration HAS_IMPLICIT_FUNC_FLAG)
-if(HAS_IMPLICIT_FUNC_FLAG)
-  set(scalapack_c_flags -Wno-implicit-function-declaration)
-endif()
-
 if(NOT TARGET LAPACK)
   # acquired by find_package instead of ExternalProject, so make dummy target
   add_custom_target(LAPACK)
@@ -51,7 +43,6 @@ ${SCALAPACK_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}scalapack${CMAKE_STATIC_LIBR
 ${SCALAPACK_ROOT}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}blacs${CMAKE_STATIC_LIBRARY_SUFFIX})
 
 set(scalapack_args
--DCMAKE_C_FLAGS=${scalapack_c_flags}
 -DCMAKE_INSTALL_PREFIX:PATH=${SCALAPACK_ROOT}
 -DLAPACK_ROOT:PATH=${LAPACK_ROOT}
 -DBUILD_SHARED_LIBS:BOOL=false
