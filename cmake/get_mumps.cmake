f6e56a106d7714987754ff274fcefefe824728d8
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-03 03:28:19 -0400

    allow version selection 5.3.1,5.4.1

--- cmake/get_mumps.cmake
+++ cmake/get_mumps.cmake
@@ -1,12 +1,16 @@
 include(FetchContent)
 
-file(READ ${CMAKE_CURRENT_LIST_DIR}/mumps.json json)
+file(READ ${CMAKE_CURRENT_LIST_DIR}/libraries.json json)
 
-string(JSON url0 GET ${json} mumps url0)
-string(JSON url1 GET ${json} mumps url1)
-string(JSON url2 GET ${json} mumps url2)
-string(JSON mumps_sha256 GET ${json} mumps sha256)
-set(mumps_urls ${url0} ${url1} ${url2})
+set(mumps_urls)
+string(JSON N LENGTH ${json} mumps ${MUMPS_UPSTREAM_VERSION} urls)
+math(EXPR N "${N}-1")
+foreach(i RANGE ${N})
+  string(JSON _u GET ${json} mumps ${MUMPS_UPSTREAM_VERSION} urls ${i})
+  list(APPEND mumps_urls ${_u})
+endforeach()
+
+string(JSON mumps_sha256 GET ${json} mumps ${MUMPS_UPSTREAM_VERSION} sha256)
 
 FetchContent_Declare(mumps
 URL ${mumps_urls}
