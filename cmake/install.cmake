8770d89da5309460d4418f76e43cc5e79d01874b
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-24 23:43:25 -0400

    install: isolate temp files

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -3,11 +3,11 @@
 include(CMakePackageConfigHelpers)
 
 configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in
-  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
+  ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}Config.cmake
   INSTALL_DESTINATION lib)
 
 write_basic_package_version_file(
-  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+  ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}ConfigVersion.cmake
   COMPATIBILITY SameMinorVersion)
 
 install(EXPORT ${PROJECT_NAME}Targets
@@ -15,8 +15,8 @@ install(EXPORT ${PROJECT_NAME}Targets
   DESTINATION lib/cmake/${PROJECT_NAME})
 
 install(FILES
-  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}Config.cmake
-  ${CMAKE_CURRENT_BINARY_DIR}/cmake/${PROJECT_NAME}ConfigVersion.cmake
+  ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}Config.cmake
+  ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}ConfigVersion.cmake
   DESTINATION lib/cmake/${PROJECT_NAME})
 
 # --- CPack
