53d64c7bfe066ff2de8798f421a70e0d6bca308c
Michael Hirsch <scivision@users.noreply.github.com>
2022-03-08 02:32:53 -0500

    patch: windows use Git Bash: greatly simplify
    The previous method of patching on Windows was rickety and
    complicated. Many Windows MUMPS users will have Git Bash,
    so use this instead. Fallback for them would be to use MUMPS
    version not needing patching such as MUMPS 5.3.5
    
    Co-authored-by: Boris Basic <baljci@hotmail.com>

--- cmake/PatchFile.cmake
+++ cmake/PatchFile.cmake
@@ -6,61 +6,33 @@
 # patch_file(in_file patch_file)
 #     Apply patch_file to in_file via GNU Patch.
 
-if(WIN32)
-  find_package(Msys)
-  if(MSYS_INSTALL_PATH)
+find_program(PATCH NAMES patch)
+
+if(WIN32 AND NOT PATCH)
+  find_package(Git)
+  if(Git_FOUND)
+    get_filename_component(GIT_DIR ${GIT_EXECUTABLE} DIRECTORY)
+    get_filename_component(GIT_DIR ${GIT_DIR} DIRECTORY)
+
     find_program(PATCH
     NAMES patch
-    HINTS ${MSYS_INSTALL_PATH}
-    PATH_SUFFIXES bin usr/bin
+    HINTS ${GIT_DIR}
+    PATH_SUFFIXES usr/bin
+    REQUIRED
     )
   endif()
-
-  if(NOT PATCH)
-    find_program(WSL NAMES wsl)
-  endif()
-else()
-  find_program(PATCH NAMES patch)
 endif()
 
-
-function(patch_file in_file patch_file out_file)
-
-if(PATCH)
-  execute_process(COMMAND ${PATCH} ${in_file} --input=${patch_file} --output=${out_file}
-  TIMEOUT 15
-  COMMAND_ERROR_IS_FATAL ANY
-  )
-elseif(WSL)
-  execute_process(COMMAND ${WSL} wslpath ${in_file}
-  TIMEOUT 5
-  OUTPUT_VARIABLE in_wsl
-  COMMAND_ERROR_IS_FATAL ANY
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-
-  execute_process(COMMAND ${WSL} wslpath ${patch_file}
-  TIMEOUT 5
-  OUTPUT_VARIABLE patch_wsl
-  COMMAND_ERROR_IS_FATAL ANY
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-
-  execute_process(COMMAND ${WSL} wslpath ${out_file}
-  TIMEOUT 5
-  OUTPUT_VARIABLE out_wsl
-  COMMAND_ERROR_IS_FATAL ANY
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
-
-  execute_process(COMMAND ${WSL} patch ${in_wsl} --input=${patch_wsl} --output=${out_wsl}
-  TIMEOUT 15
-  COMMAND_ERROR_IS_FATAL ANY
-  )
-else()
-  message(FATAL_ERROR "Could not find patch program")
+if(NOT PATCH)
+  message(FATAL_ERROR "Did not find GNU PATCH")
 endif()
-endfunction(patch_file)
 
+execute_process(COMMAND ${PATCH} ${in_file} --input=${patch_file} --output=${out_file} --ignore-whitespace
+TIMEOUT 15
+COMMAND_ECHO STDOUT
+RESULT_VARIABLE ret
+)
 
-patch_file(${in_file} ${patch_file} ${out_file})
+if(NOT ret EQUAL 0)
+  message(FATAL_ERROR "Failed to apply patch ${patch_file} to ${in_file} with ${PATCH}")
+endif()
