ee5ef1b9b406dbe10c2f5e40b940aa9e8144cd81
Michael Hirsch <scivision@users.noreply.github.com>
2022-04-14 23:08:42 -0400

    lapack 3.10.1

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -41,13 +41,11 @@ set(CMAKE_INSTALL_RPATH_USE_LINK_PATH true)
 # Necessary for shared library with Visual Studio / Windows oneAPI
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
 
-
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   # will not take effect without FORCE
   # CMAKE_BINARY_DIR for use from FetchContent
   set(CMAKE_INSTALL_PREFIX ${CMAKE_BINARY_DIR} CACHE PATH "Install top-level directory" FORCE)
 endif()
-
 
 # --- auto-ignore build directory
 if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
