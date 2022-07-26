44b9a3ab6b559f6171267558ad3d89f121c1227b
Michael Hirsch <scivision@users.noreply.github.com>
2022-07-26 12:09:39 -0400

    ci:windows: use oneAPI
    ci:oneapi-windows: CMAKE_BUILD_TYPE Release required to avoid libstdc++ iterator error

---
+++ .github/workflows/oneapi_run_windows.bat
@@ -0,0 +1,37 @@
+REM batch file since Github Actions doesn't support shell cmd well,
+REM and cmd is needed for oneAPI Windows
+
+call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
+
+echo "MKLROOT: %MKLROOT%"
+echo "I_MPI_ROOT: %I_MPI_ROOT%"
+
+echo "configure"
+cmake -B build -DCMAKE_INSTALL_PREFIX=%RUNNER_TEMP%
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "build"
+cmake --build build --parallel
+if %errorlevel% neq 0 (
+  type build\CMakeFiles\CMakeError.log & exit /b %errorlevel%
+)
+
+echo "Run tests"
+ctest --test-dir build --preset default -V
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "install project"
+cmake --install build
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "Example config"
+cmake -B example/build -S example -DCMAKE_PREFIX_PATH=%RUNNER_TEMP%
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "Example build"
+cmake --build example/build
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "Example test"
+ctest --test-dir example/build -V
+if %errorlevel% neq 0 exit /b %errorlevel%
