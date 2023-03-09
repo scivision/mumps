d57d55cf476dc866bab179683f694239cf18b4ab
scivision <scivision@users.noreply.github.com>
2023-03-09 17:20:25 -0500

    put options.cmake at top level for user visibility

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -96,3 +96,20 @@ elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
 endif()
 
 # Per MUMPS 5.4 manual section 9, not necessary to set default int64. Doing so causes problems at runtime.
+
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+
+# Rpath options necessary for shared library install to work correctly in user projects
+set(CMAKE_INSTALL_NAME_DIR ${CMAKE_INSTALL_FULL_LIBDIR})
+set(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_FULL_LIBDIR})
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH true)
+
+# Necessary for shared library with Visual Studio / Windows oneAPI
+set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
+
+# allow CMAKE_PREFIX_PATH with ~ expand
+if(CMAKE_PREFIX_PATH)
+  get_filename_component(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ABSOLUTE)
+endif()
+
+file(GENERATE OUTPUT .gitignore CONTENT "*")
