0f2343e2dccd93256cda92180224b7956a343238
adam-sim-dev <>
2023-06-30 16:07:34 +0800

    add_feature_info for METIS

--- cmake/summary.cmake
+++ cmake/summary.cmake
@@ -3,6 +3,7 @@ add_feature_info(Parallel parallel "parallel MUMPS (using MPI and Scalapack)")
 add_feature_info(64-bit-integer intsize64 "use 64-bit integers in C and Fortran")
 add_feature_info(GEMMT BLAS_HAVE_GEMMT "use GEMMT for symmetric matrix-matrix multiplication")
 
+add_feature_info(METIS metis "METIS graph partitioning https://github.com/KarypisLab/METIS")
 add_feature_info(Scotch scotch "Scotch graph partitioning https://www.labri.fr/perso/pelegrin/scotch/")
 add_feature_info(Openmp openmp "OpenMP API https://www.openmp.org/")
 
