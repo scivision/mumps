289945bd722756db2f3d1beded0f567a021032b0
scivision <scivision@users.noreply.github.com>
2023-07-14 11:24:43 -0400

    ci: CMakeConfigureLog.yaml

--- .github/workflows/oneapi_run_windows.bat
+++ .github/workflows/oneapi_run_windows.bat
@@ -10,7 +10,7 @@ echo "I_MPI_ROOT: %I_MPI_ROOT%"
 echo "configure %GITHUB_REPOSITORY%"
 cmake --preset default --install-prefix %RUNNER_TEMP%
 if %errorlevel% neq 0 (
-  type build\CMakeFiles\CMakeError.log & exit /b %errorlevel%
+  type build\CMakeFiles\CMakeConfigureLog.yaml & exit /b %errorlevel%
 )
 
 echo "workflow %GITHUB_REPOSITORY%"
