8f8c3476253c55829687e13d2d1a0d807d56ed35
scivision <scivision@users.noreply.github.com>
2023-12-20 18:01:16 -0500

    fix matlab/octave examples

--- cmake/matlab.cmake
+++ cmake/matlab.cmake
@@ -6,37 +6,40 @@ if(parallel)
   message(WARNING "MUMPS MEX assumes no MPI (cmake -Dparallel=no)")
 endif()
 
-if(matlab)
-  find_package(Matlab REQUIRED COMPONENTS MEX_COMPILER MAIN_PROGRAM)
-elseif(octave)
-  find_package(Octave REQUIRED COMPONENTS Development Interpreter)
-endif()
-
 set(mumps_matlab_path ${mumps_SOURCE_DIR}/MATLAB)
 
 if(matlab)
 
+find_package(Matlab REQUIRED COMPONENTS MEX_COMPILER MAIN_PROGRAM)
+
 matlab_add_mex(NAME dmumpsmex
 SHARED
-SRC ${mumps_SOURCE_DIR}/matlab/mumpsmex.c
+SRC ${mumps_matlab_path}/mumpsmex.c
 LINK_TO MUMPS::MUMPS
 )
 
 add_test(NAME matlabMEX
-COMMAND ${Matlab_MAIN_PROGRAM} -batch "addpath('${PROJECT_BINARY_DIR}', '${mumps_matlab_path}', '${PROJECT_SOURCE_DIR}/matlab'), example"
+COMMAND ${Matlab_MAIN_PROGRAM} -sd ${mumps_matlab_path}
+-batch "addpath('$<TARGET_FILE_DIR:dmumps>'), sparserhs_example"
 )
+set_property(TEST matlabMEX PROPERTY TIMEOUT 15)
+# sometimes the example succeeds but hangs on cleanup
 
 elseif(octave)
 
-add_library(dmumpsmex SHARED mumpsmex.c)
+find_package(Octave REQUIRED COMPONENTS Development Interpreter)
+
+add_library(dmumpsmex SHARED ${mumps_matlab_path}/mumpsmex.c)
 target_link_libraries(dmumpsmex PRIVATE MUMPS::MUMPS Octave::Octave)
 set_property(TARGET dmumpsmex PROPERTY SUFFIX .mex)
 set_property(TARGET dmumpsmex PROPERTY PREFIX "")
 
 
 add_test(NAME octaveMEX
-COMMAND ${Octave_EXECUTABLE} --eval "addpath('${PROJECT_BINARY_DIR}', '${mumps_matlab_path}', '${PROJECT_SOURCE_DIR}/matlab'), example"
+COMMAND ${Octave_EXECUTABLE} -p ${mumps_matlab_path}
+--eval "addpath('$<TARGET_FILE_DIR:dmumps>'), sparserhs_example"
 )
+set_property(TEST octaveMEX PROPERTY TIMEOUT 15)
 
 endif()
 
