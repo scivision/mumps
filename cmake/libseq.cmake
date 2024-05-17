880d2355bb0bafa9e5eee26b7ac59f4f0921f41a
scivision <scivision@users.noreply.github.com>
2024-05-16 20:05:17 -0400

    mpiseq: C,Fortran objects for compiler compatibility
    Co-authored-by: Dario Mangoni <dariomangoni@users.noreply.github.com>

--- cmake/libseq.cmake
+++ cmake/libseq.cmake
@@ -1,6 +1,18 @@
 set(_l "${mumps_SOURCE_DIR}/libseq/")
 
-add_library(mpiseq ${_l}elapse.c ${_l}mpi.f ${_l}mpic.c)
+add_library(mpiseq_C OBJECT ${_l}elapse.c ${_l}mpic.c)
+target_include_directories(mpiseq_C PUBLIC
+"$<BUILD_INTERFACE:${_l}>"
+$<INSTALL_INTERFACE:include>
+)
+
+add_library(mpiseq_FORTRAN OBJECT ${_l}mpi.f)
+target_include_directories(mpiseq_FORTRAN PUBLIC
+"$<BUILD_INTERFACE:${_l}>"
+$<INSTALL_INTERFACE:include>
+)
+
+add_library(mpiseq $<TARGET_OBJECTS:mpiseq_C> $<TARGET_OBJECTS:mpiseq_FORTRAN>)
 
 target_include_directories(mpiseq PUBLIC
 "$<BUILD_INTERFACE:${_l}>"
@@ -12,3 +24,6 @@ set_property(TARGET mpiseq PROPERTY EXPORT_NAME MPISEQ)
 install(TARGETS mpiseq EXPORT ${PROJECT_NAME}-targets)
 
 install(FILES ${_l}elapse.h ${_l}mpi.h ${_l}mpif.h TYPE INCLUDE)
+
+set(NUMERIC_INC ${_l})
+list(APPEND NUMERIC_LIBS mpiseq)
