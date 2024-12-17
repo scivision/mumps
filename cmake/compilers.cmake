f77fc4a07b39d6789e8d04e2ac1b3e8d6c2657ca
scivision <scivision@users.noreply.github.com>
2024-12-17 11:02:30 -0500

    fno-strict-aliasing for oneAPI,Clang,GNU
    simplify complier options using genex

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -5,41 +5,36 @@ add_compile_definitions("$<$<COMPILE_LANGUAGE:C>:Add_>")
 
 add_compile_definitions("$<$<BOOL:${intsize64}>:INTSIZE64;PORD_INTSIZE64>")
 
-if(CMAKE_C_COMPILER_ID MATCHES "^Intel")
-  add_compile_options($<$<COMPILE_LANGUAGE:C>:-Werror-implicit-function-declaration>)
+# --- C options
 
-  add_compile_options($<$<COMPILE_LANG_AND_ID:C,IntelLLVM>:$<IF:$<BOOL:${WIN32}>,/Qopenmp,-fiopenmp>>)
-elseif(CMAKE_C_COMPILER_ID MATCHES "Clang|GNU")
-  add_compile_options("$<$<COMPILE_LANGUAGE:C>:-Werror-implicit-function-declaration;-fno-strict-aliasing>")
-elseif(CMAKE_C_COMPILER_ID MATCHES "MSVC")
-  add_compile_options($<$<COMPILE_LANGUAGE:C>:/openmp>)
-endif()
+add_compile_options(
+"$<$<COMPILE_LANG_AND_ID:C,IntelLLVM>:$<IF:$<BOOL:${WIN32}>,/Qopenmp,-fiopenmp>>"
+"$<$<COMPILE_LANG_AND_ID:C,AppleClang,Clang,GNU,IntelLLVM>:-Werror-implicit-function-declaration;-fno-strict-aliasing>"
+"$<$<COMPILE_LANG_AND_ID:C,MSVC>:/openmp>"
+)
 
+# --- Fortran options
 
-if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
+add_compile_options(
+"$<$<COMPILE_LANG_AND_ID:Fortran,IntelLLVM>:$<IF:$<BOOL:${WIN32}>,/Qopenmp,-fiopenmp>>"
+"$<$<COMPILE_LANG_AND_ID:Fortran,IntelLLVM>:$<IF:$<BOOL:${WIN32}>,/warn:declarations;/heap-arrays,-implicitnone>>"
+"$<$<COMPILE_LANG_AND_ID:Fortran,GNU>:-fimplicit-none>"
+"$<$<COMPILE_LANG_AND_ID:Fortran,GNU>:-fno-strict-aliasing>"
+"$<$<AND:$<COMPILE_LANG_AND_ID:Fortran,GNU>,$<VERSION_GREATER_EQUAL:${CMAKE_Fortran_COMPILER_VERSION},10>>:-fallow-argument-mismatch;-fallow-invalid-boz>"
+)
+# IntelLLVM does not have -fno-strict-aliasing for Fortran
+
+if(intsize64)
   add_compile_options(
-  "$<$<COMPILE_LANGUAGE:Fortran>:$<IF:$<BOOL:${WIN32}>,/warn:declarations;/heap-arrays,-implicitnone>>"
-  $<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<BOOL:${intsize64}>>:-i8>
+    "$<$<COMPILE_LANG_AND_ID:Fortran,Intel,IntelLLVM>:-i8>"
+    "$<$<COMPILE_LANG_AND_ID:Fortran,GNU>:-fdefault-integer-8>"
   )
+  # ALL libraries must be compiled with -fdefault-integer-8, including MPI,
+  # or runtime fails
+  # See MUMPS 5.7.0 User manual about error -69
 
-  add_compile_options($<$<COMPILE_LANG_AND_ID:Fortran,IntelLLVM>:$<IF:$<BOOL:${WIN32}>,/Qopenmp,-fiopenmp>>)
 
-  if(intsize64)
-    add_compile_definitions($<$<COMPILE_LANGUAGE:Fortran>:WORKAROUNDINTELILP64MPI2INTEGER>)
-  endif()
-
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
-  add_compile_options(
-  $<$<COMPILE_LANGUAGE:Fortran>:-fimplicit-none>
-  $<$<COMPILE_LANGUAGE:Fortran>:-fno-strict-aliasing>
-  "$<$<AND:$<VERSION_GREATER_EQUAL:${CMAKE_Fortran_COMPILER_VERSION},10>,$<COMPILE_LANGUAGE:Fortran>>:-fallow-argument-mismatch;-fallow-invalid-boz>"
-  )
-
-  if(intsize64)
-    # ALL libraries must be compiled with -fdefault-integer-8, including MPI or runtime fails
-    # See MUMPS 5.7.0 User manual about error -69
-    add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-fdefault-integer-8>)
-  endif()
+  add_compile_definitions($<$<COMPILE_LANG_AND_ID:Fortran,Intel,IntelLLVM>:WORKAROUNDINTELILP64MPI2INTEGER>)
 endif()
 
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
