1a8832951bdebe329957be55b97890d9b4807981
scivision <scivision@users.noreply.github.com>
2025-05-25 00:47:37 -0400

    require MUMPS >= 5.1

--- cmake/mumps.cmake
+++ cmake/mumps.cmake
@@ -2,7 +2,7 @@ set(_mi ${mumps_SOURCE_DIR}/include/)
 set(_s ${mumps_SOURCE_DIR}/src/)
 
 # -- generated MUMPS_INTSIZE header
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.5.0)
+if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.5)
   if(MUMPS_intsize64)
     set(intsrc ${mumps_SOURCE_DIR}/src/mumps_int_def64_h.in)
   else()
@@ -28,22 +28,11 @@ endif()
 # -- Mumps COMMON
 set(COMM_SRC_Fortran ${_s}mumps_ooc_common.F ${_s}mumps_static_mapping.F)
 
-if(MUMPS_UPSTREAM_VERSION VERSION_LESS 5.0)
-  list(APPEND COMM_SRC_Fortran ${_s}mumps_part9.F)
-endif()
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.0)
-  foreach(i IN ITEMS ana_omp_m.F double_linked_list.F fac_asm_build_sort_index_ELT_m.F fac_asm_build_sort_index_m.F fac_descband_data_m.F fac_future_niv2_mod.F fac_maprow_data_m.F front_data_mgt_m.F mumps_l0_omp_m.F omp_tps_common_m.F)
-    list(APPEND COMM_SRC_Fortran ${_s}${i})
-  endforeach()
-  if(MUMPS_UPSTREAM_VERSION VERSION_LESS 5.8)
-    list(APPEND COMM_SRC_Fortran ${_s}fac_ibct_data_m.F ${_s}mumps_comm_ibcast.F)
-  endif()
-endif()
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.1)
-  foreach(i IN ITEMS ana_orderings_wrappers_m.F lr_common.F mumps_memory_mod.F)
-    list(APPEND COMM_SRC_Fortran ${_s}${i})
-  endforeach()
-endif()
+foreach(i IN ITEMS ana_omp_m.F double_linked_list.F fac_asm_build_sort_index_ELT_m.F fac_asm_build_sort_index_m.F fac_descband_data_m.F fac_future_niv2_mod.F fac_maprow_data_m.F front_data_mgt_m.F mumps_l0_omp_m.F omp_tps_common_m.F
+                   ana_orderings_wrappers_m.F lr_common.F mumps_memory_mod.F)
+  list(APPEND COMM_SRC_Fortran ${_s}${i})
+endforeach()
+
 if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.2)
   list(APPEND COMM_SRC_Fortran  ${_s}mumps_mpitoomp_m.F)
 endif()
@@ -56,42 +45,32 @@ endif()
 if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.7)
   list(APPEND COMM_SRC_Fortran ${_s}sol_ds_common_m.F)
 endif()
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.8)
+if(MUMPS_UPSTREAM_VERSION VERSION_LESS 5.8)
+  list(APPEND COMM_SRC_Fortran ${_s}fac_ibct_data_m.F ${_s}mumps_comm_ibcast.F)
+else()
   list(APPEND COMM_SRC_Fortran ${_s}lr_stats.F ${_s}mumps_comm_buffer_common.F ${_s}mumps_intr_types_common.F ${_s}mumps_load.F ${_s}sol_omp_common_m.F ${_s}tools_common_m.F)
 endif()
 
 set(COMM_OTHER_C)
 set(COMM_OTHER_Fortran)
-foreach(i IN ITEMS mumps_common.c mumps_io_basic.c mumps_io_thread.c mumps_io_err.c mumps_io.c)
+
+foreach(i IN ITEMS mumps_common.c mumps_io_basic.c mumps_io_thread.c mumps_io_err.c mumps_io.c mumps_numa.c mumps_pord.c mumps_thread.c mumps_save_restore_C.c)
   list(APPEND COMM_OTHER_C ${_s}${i})
 endforeach()
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 4.9 AND MUMPS_UPSTREAM_VERSION VERSION_LESS 5.6)
+foreach(i IN ITEMS ana_orderings.F ana_set_ordering.F ana_AMDMF.F bcast_errors.F estim_flops.F mumps_type2_blocking.F mumps_version.F mumps_print_defined.F tools_common.F)
+  list(APPEND COMM_OTHER_Fortran ${_s}${i})
+endforeach()
+
+if(MUMPS_UPSTREAM_VERSION VERSION_LESS 5.6)
   list(APPEND COMM_OTHER_C ${_s}mumps_size.c)
 endif()
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 4.9 AND MUMPS_UPSTREAM_VERSION VERSION_LESS 5.1)
-  list(APPEND COMM_OTHER_Fortran ${_s}tools_common_mod.F)
-endif()
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 4.10 AND MUMPS_UPSTREAM_VERSION VERSION_LESS 5.2)
+if(MUMPS_UPSTREAM_VERSION VERSION_LESS 5.2)
   list(APPEND COMM_OTHER_Fortran ${_s}mumps_sol_es.F)
 endif()
 if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.6)
   list(APPEND COMM_OTHER_C ${_s}mumps_addr.c)
 endif()
 
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.0)
-  foreach(i IN ITEMS ana_orderings.F ana_set_ordering.F ana_AMDMF.F bcast_errors.F estim_flops.F mumps_type2_blocking.F mumps_version.F mumps_print_defined.F tools_common.F)
-    list(APPEND COMM_OTHER_Fortran ${_s}${i})
-  endforeach()
-  list(APPEND COMM_OTHER_C ${_s}mumps_numa.c)
-endif()
-if(MUMPS_UPSTREAM_VERSION VERSION_LESS 5.1)
-  list(APPEND COMM_OTHER_C ${_s}mumps_orderings.c)
-endif()
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.1)
-  foreach(i IN ITEMS mumps_pord.c mumps_thread.c mumps_save_restore_C.c)
-    list(APPEND COMM_OTHER_C ${_s}${i})
-  endforeach()
-endif()
 if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.2)
   foreach(i IN ITEMS mumps_config_file_C.c mumps_thread_affinity.c)
     list(APPEND COMM_OTHER_C ${_s}${i})
@@ -104,24 +83,22 @@ endif()
 if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.3)
   list(APPEND COMM_OTHER_C ${_s}mumps_register_thread.c)
 endif()
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.0 AND MUMPS_UPSTREAM_VERSION VERSION_LESS 5.7)
+if(MUMPS_UPSTREAM_VERSION VERSION_LESS 5.7)
   list(APPEND COMM_OTHER_Fortran ${_s}mumps_type_size.F)
 endif()
 if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.8)
   list(APPEND COMM_OTHER_C ${_s}mumps_flytes.c)
 endif()
 
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.1)
-  if(MUMPS_scotch)
-    foreach(i IN ITEMS mumps_scotch.c mumps_scotch64.c mumps_scotch_int.c)
-      list(APPEND COMM_OTHER_C ${_s}${i})
-    endforeach()
-  endif()
-  if(MUMPS_metis OR MUMPS_parmetis)
-    foreach(i IN ITEMS mumps_metis.c mumps_metis64.c mumps_metis_int.c)
-      list(APPEND COMM_OTHER_C ${_s}${i})
-    endforeach()
-  endif()
+if(MUMPS_scotch)
+  foreach(i IN ITEMS mumps_scotch.c mumps_scotch64.c mumps_scotch_int.c)
+    list(APPEND COMM_OTHER_C ${_s}${i})
+  endforeach()
+endif()
+if(MUMPS_metis OR MUMPS_parmetis)
+  foreach(i IN ITEMS mumps_metis.c mumps_metis64.c mumps_metis_int.c)
+    list(APPEND COMM_OTHER_C ${_s}${i})
+  endforeach()
 endif()
 
 add_library(mumps_common_C OBJECT ${COMM_OTHER_C})
@@ -165,21 +142,14 @@ function(precision_source a)
 
 set(SRC_Fortran)
 set(SRC_C)
-foreach(i IN ITEMS mumps_comm_buffer.F mumps_ooc_buffer.F mumps_ooc.F mumps_struc_def.F)
+foreach(i IN ITEMS mumps_comm_buffer.F mumps_ooc_buffer.F mumps_ooc.F mumps_struc_def.F
+                   ana_aux.F ana_aux_par.F ana_lr.F fac_asm_master_ELT_m.F fac_asm_master_m.F fac_front_aux.F fac_front_LU_type1.F fac_front_LU_type2.F fac_front_LDLT_type1.F fac_front_LDLT_type2.F fac_front_type2_aux.F fac_lr.F fac_omp_m.F fac_par_m.F lr_core.F mumps_lr_data_m.F omp_tps_m.F static_ptr_m.F
+                   lr_type.F mumps_save_restore.F mumps_save_restore_files.F)
   list(APPEND SRC_Fortran ${_s}${a}${i})
 endforeach()
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.0)
-  foreach(i IN ITEMS ana_aux.F ana_aux_par.F ana_lr.F fac_asm_master_ELT_m.F fac_asm_master_m.F fac_front_aux.F fac_front_LU_type1.F fac_front_LU_type2.F fac_front_LDLT_type1.F fac_front_LDLT_type2.F fac_front_type2_aux.F fac_lr.F fac_omp_m.F fac_par_m.F lr_core.F mumps_lr_data_m.F omp_tps_m.F static_ptr_m.F)
-    list(APPEND SRC_Fortran ${_s}${a}${i})
-  endforeach()
-endif()
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.1)
-  foreach(i IN ITEMS lr_type.F mumps_save_restore.F mumps_save_restore_files.F)
-    list(APPEND SRC_Fortran ${_s}${a}${i})
-  endforeach()
-  if(MUMPS_UPSTREAM_VERSION VERSION_LESS 5.8)
-    list(APPEND SRC_Fortran ${_s}${a}lr_stats.F)
-  endif()
+
+if(MUMPS_UPSTREAM_VERSION VERSION_LESS 5.8)
+  list(APPEND SRC_Fortran ${_s}${a}lr_stats.F)
 endif()
 if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.2)
   foreach(i IN ITEMS fac_mem_dynamic.F mumps_config_file.F mumps_sol_es.F sol_lr.F)
@@ -200,41 +170,35 @@ else()
   list(APPEND SRC_Fortran ${_s}${a}fac_compact_factors_m.F ${_s}${a}mumps_intr_types.F)
 endif()
 
-if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.0)
-  foreach(i IN ITEMS
-    ini_driver.F ana_driver.F fac_driver.F
-    sol_driver.F
-    end_driver.F ana_aux_ELT.F ana_dist_m.F ana_LDLT_preprocess.F
-    ana_reordertree.F arrowheads.F bcast_int.F fac_asm_ELT.F
-    fac_asm.F fac_b.F fac_distrib_distentry.F fac_distrib_ELT.F fac_lastrtnelind.F
-    fac_mem_alloc_cb.F fac_mem_compress_cb.F fac_mem_free_block_cb.F
-    fac_mem_stack_aux.F fac_mem_stack.F
-    fac_process_band.F fac_process_blfac_slave.F fac_process_blocfacto_LDLT.F fac_process_blocfacto.F
-    fac_process_bf.F fac_process_end_facto_slave.F
-    fac_process_contrib_type1.F fac_process_contrib_type2.F fac_process_contrib_type3.F
-    fac_process_maprow.F fac_process_master2.F fac_process_message.F
-    fac_process_root2slave.F fac_process_root2son.F fac_process_rtnelind.F fac_root_parallel.F
-    fac_scalings.F fac_determinant.F fac_scalings_simScaleAbs.F fac_scalings_simScale_util.F
-    fac_sol_pool.F fac_type3_symmetrize.F ini_defaults.F
-    mumps_driver.F mumps_f77.F mumps_iXamax.F
-    ana_mtrans.F ooc_panel_piv.F rank_revealing.F
-    sol_aux.F sol_bwd_aux.F sol_bwd.F sol_c.F sol_fwd_aux.F sol_fwd.F sol_matvec.F
-    sol_root_parallel.F tools.F type3_root.F
-  )
-    list(APPEND SRC_Fortran ${_s}${a}${i})
-  endforeach()
-endif()
+foreach(i IN ITEMS
+  ini_driver.F ana_driver.F fac_driver.F
+  sol_driver.F
+  end_driver.F ana_aux_ELT.F ana_dist_m.F ana_LDLT_preprocess.F
+  ana_reordertree.F arrowheads.F bcast_int.F fac_asm_ELT.F
+  fac_asm.F fac_b.F fac_distrib_distentry.F fac_distrib_ELT.F fac_lastrtnelind.F
+  fac_mem_alloc_cb.F fac_mem_compress_cb.F fac_mem_free_block_cb.F
+  fac_mem_stack_aux.F fac_mem_stack.F
+  fac_process_band.F fac_process_blfac_slave.F fac_process_blocfacto_LDLT.F fac_process_blocfacto.F
+  fac_process_bf.F fac_process_end_facto_slave.F
+  fac_process_contrib_type1.F fac_process_contrib_type2.F fac_process_contrib_type3.F
+  fac_process_maprow.F fac_process_master2.F fac_process_message.F
+  fac_process_root2slave.F fac_process_root2son.F fac_process_rtnelind.F fac_root_parallel.F
+  fac_scalings.F fac_determinant.F fac_scalings_simScaleAbs.F fac_scalings_simScale_util.F
+  fac_sol_pool.F fac_type3_symmetrize.F ini_defaults.F
+  mumps_driver.F mumps_f77.F mumps_iXamax.F
+  ana_mtrans.F ooc_panel_piv.F rank_revealing.F
+  sol_aux.F sol_bwd_aux.F sol_bwd.F sol_c.F sol_fwd_aux.F sol_fwd.F sol_matvec.F
+  sol_root_parallel.F tools.F type3_root.F
+)
+  list(APPEND SRC_Fortran ${_s}${a}${i})
+endforeach()
+
 if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.2)
   list(APPEND SRC_Fortran ${_s}${a}sol_distrhs.F)
   list(APPEND SRC_C ${_s}${a}mumps_gpu.c)
 endif()
 if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.7)
   list(APPEND SRC_Fortran ${_s}${a}sol_distsol.F ${_s}${a}fac_diag.F ${_s}${a}fac_dist_arrowheads_omp.F)
-endif()
-if(MUMPS_UPSTREAM_VERSION VERSION_LESS 5.0)
-  foreach(i IN ITEMS mumps_part1.F mumps_part2.F mumps_part3.F mumps_part4.F mumps_part5.F mumps_part6.F mumps_part7.F mumps_part8.F)
-    list(APPEND SRC_Fortran ${_s}${a}${i})
-  endforeach()
 endif()
 
 set(CINT_SRC ${_s}mumps_c.c)
--- scripts/test_mumps_versions.cmake
+++ scripts/test_mumps_versions.cmake
@@ -2,22 +2,22 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/tempdir.cmake)
 
+# MUMPS 4.8.4, 4.9.2, 5.0.2 source no longer available from mumps-solver.org
 
-foreach(u IN ITEMS 4.8.4 4.9.2 5.0.2 5.1.2 5.2.1 5.3.5 5.4.1 5.5.1 5.6.2 5.7.3)
+set(vers 5.1.2 5.2.1 5.3.5 5.4.1 5.5.1 5.6.2 5.7.3 5.8.0)
+
+foreach(u IN LISTS vers)
   get_temp_dir(bindir)
   message(STATUS "Testing MUMPS ${u} in ${bindir}")
 
   execute_process(COMMAND ${CMAKE_COMMAND} -B${bindir} -S${CMAKE_CURRENT_LIST_DIR}/.. -DMUMPS_UPSTREAM_VERSION=${u}
   RESULT_VARIABLE ret
+  COMMAND_ERROR_IS_FATAL ANY
   )
-  if(NOT ret EQUAL 0)
-    message(SEND_ERROR "MUMPS ${u} failed to configure in ${bindir}")
-    continue()
-  endif()
 
-  execute_process(COMMAND ${CMAKE_COMMAND} --build ${bindir} RESULT_VARIABLE ret)
-  if(NOT ret EQUAL 0)
-    message(SEND_ERROR "MUMPS ${u} failed to build in ${bindir}")
-    continue()
-  endif()
+  execute_process(COMMAND ${CMAKE_COMMAND} --build ${bindir}
+  RESULT_VARIABLE ret
+  COMMAND_ERROR_IS_FATAL ANY
+  )
+
 endforeach()
