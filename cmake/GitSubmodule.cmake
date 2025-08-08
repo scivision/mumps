7bb12d4ce64af50843e78c05443d797acd855a5a
Michael Hirsch <scivision@users.noreply.github.com>
2025-08-08 04:14:01 -0400

    use parmetis dir tree

--- cmake/GitSubmodule.cmake
+++ cmake/GitSubmodule.cmake
@@ -2,16 +2,27 @@ function(git_submodule submod_dir)
 # get/update Git submodule directory to CMake, assuming the
 # Git submodule directory is a CMake project.
 
+set(workdir ${submod_dir}/..)
+cmake_path(NORMAL_PATH workdir)
+
+cmake_path(RELATIVE_PATH workdir BASE_DIRECTORY ${PROJECT_SOURCE_DIR} OUTPUT_VARIABLE submod_rel_path)
+if(NOT (submod_rel_path MATCHES ".." OR submod_rel_path STREQUAL "."))
+  message(FATAL_ERROR "Submodule ${submod_dir} is not under the project source ${PROJECT_SOURCE_DIR}")
+  return()
+endif()
+
 if(EXISTS ${submod_dir}/CMakeLists.txt)
   return()
 endif()
 
-set(CMAKE_EXECUTE_PROCESS_COMMAND_ECHO STDOUT)
-
 find_package(Git REQUIRED)
 
+set(CMAKE_EXECUTE_PROCESS_COMMAND_ECHO STDOUT)
+
+
+
 # EXISTS, do not use IS_DIRECTORY as in submodule .git is a file not a directory
-if(EXISTS ${PROJECT_SOURCE_DIR}/.git)
+if(EXISTS ${submod_dir}/../.git)
   message(DEBUG "${PROJECT_SOURCE_DIR} is a Git repository, updating submodule ${submod_dir}")
   execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${submod_dir}
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
